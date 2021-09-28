8ded33e1b7695a7181a78226eef06e525975aabe
Benji Altman <benji@altmansoftwaredesign.com>
2021-09-28 14:29:13 -0500

    some work, currently doesn't build

--- Notes/Notes/Views/NotesPage.xaml.cs
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -1,38 +1,80 @@
 ﻿using System;
 using System.IO;
+using Notes.Models;
 using Xamarin.Forms;
 
 namespace Notes.Views
 {
-	public partial class NotesPage : ContentPage
-	{
-		string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public string ItemId
+        {
+            set
+            {
+                LoadNote(value);
+            }
+        }
 
-		public NotesPage()
-		{
-			InitializeComponent();
+        public NoteEntryPage()
+        {
+            InitializeComponent();
 
-			// Read the file.
-			if (File.Exists(_fileName))
-			{
-				editor.Text = File.ReadAllText(_fileName);
-			}
-		}
+            // Set the BindingContext of the page to a new Note.
+            BindingContext = new Note();
+        }
 
-		void OnSaveButtonClicked(object sender, EventArgs e)
-		{
-			// Save the file.
-			File.WriteAllText(_fileName, editor.Text);
-		}
+        void LoadNote(string filename)
+        {
+            try
+            {
+                // Retrieve the note and set it as the BindingContext of the page.
+                Note note = new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                };
+                BindingContext = note;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load note.");
+            }
+        }
 
-		void OnDeleteButtonClicked(object sender, EventArgs e)
-		{
-			// Delete the file.
-			if (File.Exists(_fileName))
-			{
-				File.Delete(_fileName);
-			}
-			editor.Text = string.Empty;
-		}
-	}
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                // Save the file.
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Update the file.
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            // Navigate backwards
+            await Shell.Current.GoToAsync("..");
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            // Delete the file.
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+
+            // Navigate backwards
+            await Shell.Current.GoToAsync("..");
+        }
+    }
 }
